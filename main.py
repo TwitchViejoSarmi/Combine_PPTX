21d9f831d149fffd98a68eea6a17a7f84ad3a993
TwitchViejoSarmi <alesansarive@gmail.com>
2024-08-08 23:15:27 -0500

    MOD: Intentando corregir copia de textboxs.

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ import os
 
 load_dotenv()
 
-path = os.getenv('PATH')
+path = "C:/Prueba"
 
 def copy_slide(new_pptx, ex_slide):
     slide_layout = ex_slide.slide_layout
     new_slide = new_pptx.slides.add_slide(slide_layout)
-
     for shape in ex_slide.shapes:
-        new_shape = new_slide.shapes.add_shape(shape.auto_shape_type, shape.left, shape.top, shape.width, shape.height)
-        if shape.has_text_frame:
-            new_shape.text = shape.text_frame.text
+        if shape.shape_type == 14:  # Text Boxes
+                new_shape = new_slide.shapes.add_textbox(
+                    shape.left, shape.top, shape.width, shape.height
+                )
+                if shape.has_text_frame:
+                    new_shape.text = shape.text_frame.text
+
+        else:
+            new_shape = new_slide.shapes.add_shape(shape.auto_shape_type, shape.left, shape.top, shape.width, shape.height)
+            if shape.has_text_frame:
+                new_shape.text = shape.text_frame.text
 
 def copy_pptx():
     new_pptx = Presentation()
@@ -22,3 +29,7 @@ def copy_pptx():
             act_pptx = Presentation(f'{root}/{file}')
             for slide in act_pptx.slides:
                 copy_slide(new_pptx, slide)
+
+    new_pptx.save('output/output.pptx')
+
+copy_pptx()
